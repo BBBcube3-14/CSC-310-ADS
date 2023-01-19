f8cd39ef0ce23a45bec1aedab7438d5d7507b417
BBBcube3-14 <benjaminbbatie@gmail.com>
2023-01-19 16:50:46 -0600

    Int Only

--- solution.cpp
+++ solution.cpp
@@ -1,8 +1,93 @@
-#include "solutionHeader.hpp"
+//#include "solutionHeader.hpp"
 #include <iostream>
 #include <string>
 using namespace std;
+/************************************Template Code --- bunch of errors***********************************************************/
+template<typename T>
 
+struct node{
+    T data;
+    //int data;
+    node* next;
+};
+
+void insert_front(node*& head, T d){
+    node* newNode = new node;
+    newNode->data = d;
+    newNode->next = head;
+    head = newNode;
+    return;
+}
+
+void insert_back(node*& head, T d){
+    if(head == nullptr){
+        node* newNode = new node;
+        newNode->data = d;
+        newNode->next = nullptr;
+        head = newNode;
+        return;
+    }else{
+        node* newNode = new node;
+        newNode->data = d;
+        node* ptr = head;
+        while(ptr->next!=nullptr){
+            ptr = ptr->next;
+        }
+        newNode->data = d;
+        ptr->next = newNode;
+    }
+    return;
+}
+
+void print_list(node* head){
+    node* ptr = head;
+    while(ptr != nullptr){
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+    return;
+}
+
+void search_list(node* head, T d){
+    node* ptr = head;
+    while(ptr != nullptr && ptr->data != d){
+        ptr = ptr->next;
+    }
+    if(ptr == nullptr){
+        cout << "Index not found";
+    }else{
+        cout << "Index found";
+    }
+    cout << endl;
+    return;
+}
+void remove_front(node*& head){
+    if(head == nullptr){
+        cout << "Index is empty. Canceling deletion.\n";
+        return;
+    }
+    node* ptr = head;
+    head = head->next;
+    delete(ptr);
+    return;
+}
+
+void remove_back(node*& head){
+    if(head == nullptr){
+        cout << "Index is empty. Canceling deletion.\n";
+        return;
+    }
+    node* ptr = head;
+    while(ptr->next->next != nullptr){
+        ptr = ptr->next;
+    }
+    delete(ptr->next);
+    ptr->next = nullptr;
+    return;
+}
+
+/************************************Template Code --- bunch of errors***********************************************************/
 /****Not using template code that works. See header file for error causing template code.****/
 //template<typename T>
 /*
